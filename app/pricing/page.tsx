2228c809e5c63f365a12988977ef34a8a2c651d3
PersianDavinci <persiandavinci@gmail.com>
2023-11-08 19:25:41 +0330

    adding pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,20 @@
+const PricingPage = () => {
+  return (
+    <div className="isolate overflow-hidden bg-gray-900">
+      <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
+        <div className="mx-auto max-w-4xl">
+          <h2 className="text-base font-semibold leading-7 text-indigo-400">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+            The right price for you,{" "}
+            <br className="hidden sm:inline lg:hidden" />
+            whoever you are
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PricingPage;
