d49cc27c0f97e57790ef2506dd5b3a60ad452827
PersianDavinci <persiandavinci@gmail.com>
2023-11-09 17:11:12 +0330

    add auth keys

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { adminDb } from "./firebase-admin";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -11,4 +13,5 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  adapter: FirestoreAdapter(adminDb),
 } satisfies NextAuthOptions;
