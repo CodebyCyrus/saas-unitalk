b644599dfcab42c0350e28b1f77d29a4e7f6040a
PersianDavinci <persiandavinci@gmail.com>
2023-11-09 15:20:36 +0330

    Pricing card add buttons

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,9 +1,11 @@
 import { CheckIcon } from "lucide-react";
+import Link from "next/link";
+import CheckoutButton from "./CheckoutButton";
 
 const tiers = [
   {
     name: "Starter",
-    id: "starter_ID",
+    id: null,
     href: "#",
     priceMonthly: null,
     desciption: "Get chatting right away with anyone, anywhere!",
@@ -83,6 +85,17 @@ export const PricingCards = ({ redirect }: { redirect: boolean }) => {
                 ))}
               </ul>
             </div>
+            {redirect ? (
+              <Link
+                href="/register"
+                className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80"
+              >
+                {" "}
+                Get Started Today{" "}
+              </Link>
+            ) : (
+              tier.id && <CheckoutButton />
+            )}
           </div>
         ))}
       </div>
